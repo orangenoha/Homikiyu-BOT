a492d56a8e8fa2753b9786d378eba71c7897a59a
orangenoha <46075727+orangenoha@users.noreply.github.com>
2018-12-22 17:41:44 -0600

    Homikiyu BOT

---
+++ app.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+
+
+const client = new Discord.Client();
+
+const token = process.env.token;
+
+const PREFIX = ">>"
+
+client.on("ready", () => {
+
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+
+  if(message.author.bot) return;
+
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+
+  
+  if(command === "ping") {
+
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
+  if(command === "say") {
+
+    const sayMessage = args.join(" ");
+  
+    message.delete().catch(O_o=>{}); 
+
+    message.channel.send(sayMessage);
+  }
+  
+ 
+  }
+});
+
+client.login(token).catch(err => console.log(err));
